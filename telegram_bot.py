f94b7b1e3eab15b2d8bc8ddc9acce112f9115757
petrovskyd <petrovsky.dv@gmail.com>
2021-02-11 13:31:09 +0300

    добавил вопросы. ответы, на вопросы

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,9 +2,9 @@ import logging
 import os
 import random
 
-import telegram
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from dotenv import load_dotenv
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, RegexHandler
 
 import main
 from redis_db import db_connection
@@ -12,11 +12,17 @@ from utils import TelegramBotHandler
 
 logger = logging.getLogger(__name__)
 QUIZ = main.main()
+QUESTION, ANSWER = range(2)
 
 
 def start(bot, update):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Привет! Я бот для викторин')
+    custom_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счет']]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    update.message.reply_text(
+        'Привет! Я бот для викторин.\nНажмите "Новый вопрос" для начала викторины.\n/cancel - для отмены.',
+        reply_markup=reply_markup)
+    return QUESTION
 
 
 def help_command(bot, update):
@@ -26,11 +32,9 @@ def help_command(bot, update):
 
 def repeat_text(bot, update):
     """Echo the user message."""
-    custom_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счет']]
-    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
     if update.message.text == 'Новый вопрос':
         message_text = random.choice(list(QUIZ.keys()))
-        update.message.reply_text(message_text, reply_markup=reply_markup)
+        update.message.reply_text(message_text)
         db_connection.set(update.message.from_user["id"], message_text)
     else:
         answer = update.message.text
@@ -43,8 +47,54 @@ def repeat_text(bot, update):
             message_text = 'Правильно! Поздравляю! Для следующего вопроса нажми "Новый вопрос"'
         else:
             message_text = 'Неправильно… Попробуешь ещё раз?'
-        update.message.reply_text(message_text, reply_markup=reply_markup)
+        update.message.reply_text(message_text)
 
+
+def handle_new_question_request(bot, update):
+    message_text = random.choice(list(QUIZ.keys()))
+    update.message.reply_text(message_text)
+    db_connection.set(update.message.from_user["id"], message_text)
+    correct_answer = QUIZ[message_text].lower().strip()
+    correct_answer = correct_answer.split('.')[0]
+    correct_answer = correct_answer.split('(')[0].strip()
+    print(f'правильный ответ: {correct_answer}')
+    return ANSWER
+
+
+def handle_solution_attempt(bot, update):
+    answer = update.message.text
+    question = db_connection.get(update.message.from_user["id"])
+    correct_answer = QUIZ[question].lower().strip()
+    correct_answer = correct_answer.split('.')[0]
+    correct_answer = correct_answer.split('(')[0].strip()
+    print(f'правильный ответ: {correct_answer}')
+    if answer.strip().lower() == correct_answer:
+        message_text = 'Правильно! Поздравляю! Для следующего вопроса нажми "Новый вопрос"'
+        update.message.reply_text(message_text)
+        return QUESTION
+    else:
+        message_text = 'Неправильно… Попробуешь ещё раз?'
+        update.message.reply_text(message_text)
+        return ANSWER
+
+
+def handle_show_answer(bot, update):
+    question = db_connection.get(update.message.from_user["id"])
+    correct_answer = QUIZ[question].lower().strip()
+    correct_answer = correct_answer.split('.')[0]
+    correct_answer = correct_answer.split('(')[0].strip()
+    print(f'правильный ответ: {correct_answer}')
+    update.message.reply_text(f'правильный ответ: {correct_answer}')
+    return QUESTION
+
+
+def cancel(bot, update):
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text('Bye! I hope we can talk again some day.',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
 
 
 def error_handler(bot, update, error):
@@ -68,9 +118,35 @@ def main():
     updater.logger.addHandler(logger_handler)
     dispatcher = updater.dispatcher
     dispatcher.logger.addHandler(logger_handler)
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CommandHandler('help', help_command))
-    dispatcher.add_handler(MessageHandler(Filters.text, repeat_text))
+    # dispatcher.add_handler(CommandHandler('start', start))
+    # dispatcher.add_handler(CommandHandler('help', help_command))
+    # dispatcher.add_handler(MessageHandler(Filters.text, repeat_text))
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            QUESTION: [RegexHandler('^Новый вопрос$', handle_new_question_request),
+                       CommandHandler('cancel', cancel)],
+
+            ANSWER: [RegexHandler('^Сдаться$', handle_show_answer),
+                     MessageHandler(Filters.text, handle_solution_attempt),
+                     CommandHandler('cancel', cancel)]
+
+            # PHOTO: [MessageHandler(Filters.photo, photo),
+            #         CommandHandler('skip', skip_photo)],
+            #
+            # LOCATION: [MessageHandler(Filters.location, location),
+            #            CommandHandler('skip', skip_location)],
+            #
+            # BIO: [MessageHandler(Filters.text, bio)]
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    dispatcher.add_handler(conv_handler)
+
     dispatcher.add_error_handler(error_handler)
     updater.start_polling()
     updater.idle()
